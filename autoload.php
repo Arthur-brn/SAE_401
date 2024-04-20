0bc2ecab11dedbab598a4617691cc683df1badef
Clément Menou <clementmenou0@gmail.com>
2024-04-20 16:01:22 +0200

    Laravel project setup

--- autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Autoload function
-spl_autoload_register(function ($className) {
-    // Convertir les anti-slashes en slashes
-    $className = str_replace("\\", "/", $className);
-
-    // Inclure le fichier de classe
-    include __DIR__ . "/$className.php";
-});
