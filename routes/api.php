378fd922c41fea61debac7201507aa289c04d07f
Clément Menou <clementmenou0@gmail.com>
2024-05-14 11:35:26 +0200

    Exemple book controller use

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Controller
+use App\Http\Controllers\BookController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::post('/books', [BookController::class, 'store']);
+Route::put('/books/{id}', [BookController::class, 'update']);
+Route::delete('/books/{id}', [BookController::class, 'destroy']);
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,6 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-Route::post('/books', [BookController::class, 'store']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'destroy']);
+Route::get('/books', function () {
+    return view('books.index');
+});
