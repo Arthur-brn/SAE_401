c481a5ed4fca9f4c9a3d5751a25e7d8572908594
Arthur-brn <cybarthur@gmail.com>
2024-05-17 14:11:13 +0200

    layout updates

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{{ $fileCss }}</title>
-        <link rel="stylesheet" href="./css/header.css" />
+        <link rel="stylesheet" href="./css/layout.css" />
         <link rel="stylesheet" href="./css/{{ $fileCss }}.css" />
         <link rel="stylesheet" href="./css/app.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -52,7 +52,28 @@
             @yield('content')
         </main>
         <footer>
-
+            <div>
+                <div id="footer_left">
+                    <p>TELOCULTURE</p>
+                    <p>Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <div id="icons">
+                        <i class="fa-brands fa-facebook fa-2xl" style="color: #ff625c;"></i>
+                        <i class="fa-brands fa-linkedin fa-2xl" style="color: #ff625c;"></i>
+                        <i class="fa-brands fa-twitter fa-2xl" style="color: #ff625c;"></i>
+                        <i class="fa-brands fa-youtube fa-2xl" style="color: #ff625c;"></i>
+                    </div>
+                </div>
+                <div id="footer_right">
+                    <p id="footer_title">Catégories</p>
+                    <a href="#">ACCUEIL</a>
+                    <a href="#">LITTÉRATURE</a>
+                    <a href="#">CINÉMA</a>
+                    <a href="#">MUSIQUES</a>
+                    <a href="#">TENDANCES</a>
+                    <a href="#">CONTACTEZ-NOUS</a>
+                </div>
+            </div>
+            <div></div>
         </footer>
     </body>
 </html>
