c51cdcfad88e716e1876d2ccf2760ce1763a960a
Arthur-brn <cybarthur@gmail.com>
2024-05-17 10:39:17 +0200

    front home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,60 @@
 @extends('layout')
 
 @section('content')
+<section id="top_page">
+    <div id="top_left">
+        <h1>Nos dernières pépites</h1>
+        <p class="p1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut magna velit eleifend. Amet, quis urna, a eu.</p>
+        <button>VOIR PLUS <i class="fa-solid fa-arrow-right" style="color: #6887f6;"></i></button>
+    </div>
+    <div id="top_right">
+        <div class="img"></div>
+    </div>
+</section>
+<section id="literature">
+    <div>
+        <div>
+            <span>-Littérature</span>
+            <h2>Découvrez nos catégories !</h2>
+            <div>
+                <svg class="arrow" id="left_arrow" style="enable-background:new 0 0 1024 1024;" version="1.1" viewBox="0 0 1024 1024" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="#FFFFFF" stroke="#8C8C8C" stroke-width="20" d="M73,512c0,59.3,11.6,116.8,34.5,170.9c22.1,52.3,53.8,99.2,94.1,139.5s87.3,72,139.5,94.1  C395.3,939.4,452.7,951,512,951s116.8-11.6,170.9-34.5c52.3-22.1,99.2-53.8,139.5-94.1s72-87.3,94.1-139.5  C939.4,628.8,951,571.3,951,512s-11.6-116.8-34.5-170.9c-22.1-52.3-53.8-99.2-94.1-139.5c-40.3-40.3-87.3-72-139.5-94.1  C628.8,84.6,571.3,73,512,73s-116.8,11.6-170.9,34.5c-52.3,22.1-99.2,53.8-139.5,94.1c-40.3,40.3-72,87.3-94.1,139.5  C84.6,395.3,73,452.7,73,512z M312,492h351.7L536.2,365.8l28.1-28.4L740.8,512L564.2,686.6l-28.1-28.4L663.6,532H312V492z" id="XMLID_40_"/><g id="XMLID_1_"/><g id="XMLID_2_"/><g id="XMLID_3_"/><g id="XMLID_4_"/><g id="XMLID_5_"/></svg>
+                <svg class="arrow" id="right_arrow" style="enable-background:new 0 0 1024 1024;" version="1.1" viewBox="0 0 1024 1024" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="#FF625C" d="M73,512c0,59.3,11.6,116.8,34.5,170.9c22.1,52.3,53.8,99.2,94.1,139.5s87.3,72,139.5,94.1  C395.3,939.4,452.7,951,512,951s116.8-11.6,170.9-34.5c52.3-22.1,99.2-53.8,139.5-94.1s72-87.3,94.1-139.5  C939.4,628.8,951,571.3,951,512s-11.6-116.8-34.5-170.9c-22.1-52.3-53.8-99.2-94.1-139.5c-40.3-40.3-87.3-72-139.5-94.1  C628.8,84.6,571.3,73,512,73s-116.8,11.6-170.9,34.5c-52.3,22.1-99.2,53.8-139.5,94.1c-40.3,40.3-72,87.3-94.1,139.5  C84.6,395.3,73,452.7,73,512z M312,492h351.7L536.2,365.8l28.1-28.4L740.8,512L564.2,686.6l-28.1-28.4L663.6,532H312V492z" id="XMLID_40_"/><g id="XMLID_1_"/><g id="XMLID_2_"/><g id="XMLID_3_"/><g id="XMLID_4_"/><g id="XMLID_5_"/></svg>
+            </div>
+        </div>
+        <div>
+            <p class="p2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut magna velit eleifend. Amet, quis urna, a eu.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum..</p>
+        </div>
+    </div>
+    <div id="books_categories">
+        <div class="categorie">
+            <div class="img"></div>
+            <h3>Roman</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut mat,</p>
+        </div>
+        <div class="categorie">
+            <div class="img"></div>
+            <h3>Roman</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut mat,</p>
+        </div>
+        <div class="categorie">
+            <div class="img"></div>
+            <h3>Roman</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut mat,</p>
+        </div>
+    </div>
+    <button>VOIR PLUS <i class="fa-solid fa-arrow-right" style="color: #6887f6;"></i></button>
+</section>
+<section id="cinema">
+    <div id="left_cine">
+        <span>-Littérature</span>
+        <h2>Les dernières sorties au cinéma en DVD !</h2>
+        <p class="p2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut magna velit eleifend. Amet, quis urna, a eu.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum..</p>
+        <button>VOIR PLUS <i class="fa-solid fa-arrow-right" style="color: #6887f6;"></i></button>
+    </div>
+    <div id="right_cine">
+        <div class="img">
 
+        </div>
+    </div>
+</section>
 @endsection
