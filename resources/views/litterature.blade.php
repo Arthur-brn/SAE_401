9e44acab075f74d8eea12e6054c8e769fcaeeeee
Alexandre <alexandre.testa.at@gmail.com>
2024-06-03 11:04:04 +0200

    essayer de debug

--- resources/views/litterature.blade.php
+++ resources/views/litterature.blade.php
@@ -191,6 +191,31 @@
 </section>
 
 <script src="./js/splide-4.1.3/dist/js/splide.min.js"></script>
-<script src="./js/litterature.js"></script>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        function getSplideOptions() {
+            return window.innerWidth > 480 ?
+                {
+                    perPage: 5,
+                    type: "loop",
+                    padding: "0 5rem"
+                } :
+                {
+                    perPage: 1,
+                    type: "loop",
+                    padding: "0 5rem"
+                };
+        }
+
+        var splides = document.querySelectorAll(".splide");
+        splides.forEach(function(splideElement) {
+            var splide = new Splide(splideElement, getSplideOptions()).mount();
+
+            window.addEventListener("resize", function() {
+                splide.options = getSplideOptions();
+            });
+        });
+    });
+</script>
 
 @endsection
