bcc3f3a06e1b523bfaaac2e2fec978822b3cc49d
Clément Menou <clementmenou0@gmail.com>
2024-06-10 18:44:07 +0200

    seeders fixes

--- database/seeders/DirectorSeeder.php
+++ database/seeders/DirectorSeeder.php
@@ -17,19 +17,16 @@ class DirectorSeeder extends Seeder
         DB::table('director')->insert([
             [
                 'name' => 'Steven Spielberg',
-                'picture' => 'steven_spielberg.jpg',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'name' => 'Christopher Nolan',
-                'picture' => 'christopher_nolan.jpg',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'name' => 'Quentin Tarantino',
-                'picture' => 'quentin_tarantino.jpg',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
