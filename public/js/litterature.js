de98399181caa567907229dab80ee2b6f34d893c
Alexandre <alexandre.testa.at@gmail.com>
2024-06-11 14:25:46 +0200

    réglage css responsive

--- public/js/litterature.js
+++ public/js/litterature.js
@@ -26,40 +26,49 @@ document.addEventListener("DOMContentLoaded", function () {
         getSplideOptions()
     ).mount();
 
+    window.addEventListener("resize", function () {
+        nouveautesSplide.options = getSplideOptions();
+        nouveautesSplide.refresh();
+        coupsDeCoeurSplide.options = getSplideOptions();
+        coupsDeCoeurSplide.refresh();
+    });
+
     // Fonction pour récupérer les informations sur le livre le plus emprunté
     function fetchMostLoanedBook() {
-        fetch('/api/books-most-loaned')
-            .then(response => response.json())
-            .then(data => {
+        fetch("/api/books-most-loaned")
+            .then((response) => response.json())
+            .then((data) => {
                 // Mettre à jour les éléments HTML avec les données du livre le plus emprunté
-                const topPageSection = document.getElementById('top-page');
-                const titleElement = topPageSection.querySelector('h2');
-                const paragraphElement = topPageSection.querySelector('p');
-                const detailPageLink = topPageSection.querySelector('a');
-                const imgElement = topPageSection.querySelector('.img img');
-            
+                const topPageSection = document.getElementById("top-page");
+                const titleElement = topPageSection.querySelector("h2");
+                const paragraphElement = topPageSection.querySelector("p");
+                const detailPageLink = topPageSection.querySelector("a");
+                const imgElement = topPageSection.querySelector(".img img");
+
                 titleElement.textContent = data.title;
                 paragraphElement.textContent = data.summary;
                 detailPageLink.href = "/litterature-" + data.id;
                 imgElement.src = "./assets/img/livres/" + data.picture;
                 imgElement.alt = data.title;
             })
-            .catch(error => console.error('Error fetching most loaned book:', error));
+            .catch((error) =>
+                console.error("Error fetching most loaned book:", error)
+            );
     }
 
     function fetchBooks(url, listElementId) {
         fetch(url)
-            .then(response => response.text())
-            .then(text => {
-                console.log('Raw response:', text);
-                return JSON.parse(text);  // Manually parse JSON
+            .then((response) => response.text())
+            .then((text) => {
+                console.log("Raw response:", text);
+                return JSON.parse(text); // Manually parse JSON
             })
-            .then(data => {
+            .then((data) => {
                 const listElement = document.getElementById(listElementId);
-                listElement.innerHTML = '';
-                data.forEach(book => {
-                    const listItem = document.createElement('li');
-                    listItem.className = 'splide__slide';
+                listElement.innerHTML = "";
+                data.forEach((book) => {
+                    const listItem = document.createElement("li");
+                    listItem.className = "splide__slide";
                     listItem.innerHTML = `
                         <a href="/litterature-${book.id}">
                             <img src="./assets/img/livres/${book.picture}" alt="${book.title}">
@@ -71,19 +80,15 @@ document.addEventListener("DOMContentLoaded", function () {
                     `;
                     listElement.appendChild(listItem);
                 });
-                new Splide(`#${listElementId.split('-')[0]}`, getSplideOptions()).mount();
+                new Splide(
+                    `#${listElementId.split("-")[0]}`,
+                    getSplideOptions()
+                ).mount();
             })
-            .catch(error => console.error('Error fetching books:', error));
+            .catch((error) => console.error("Error fetching books:", error));
     }
 
     fetchMostLoanedBook();
-    fetchBooks('/api/books-latest', 'nouveautes-list');
-    fetchBooks('/api/books-most-loaned-books', 'coups_de_coeur-list');
-    
-    window.addEventListener("resize", function () {
-        nouveautesSplide.options = getSplideOptions();
-        coupsDeCoeurSplide.options = getSplideOptions();
-    });
+    fetchBooks("/api/books-latest", "nouveautes-list");
+    fetchBooks("/api/books-most-loaned-books", "coups_de_coeur-list");
 });
-
-
